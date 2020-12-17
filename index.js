7916a7033662f2094a1862920b448abd77fc639a
Charbel t <charbel2000.52@gmail.com>
2020-12-17 10:35:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function BoardMember(name,homeState,training){
   this.training=training;
 }
 BoardMember.prototype.veto = ()=>'No, I must disagree';
-BoardMember.prototype.approve= ()=>'You cam do that!';
+BoardMember.prototype.approve= ()=>'You can do that!';
 BoardMember.prototype.doCharity= ()=>'I like to help people';
 BoardMember.prototype.releasePressStatement= ()=>'You will see great things from Scuber.';
 BoardMember.prototype.sayHi= ()=>`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`;
