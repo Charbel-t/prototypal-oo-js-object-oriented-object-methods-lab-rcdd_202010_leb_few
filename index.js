29a2f55f5fc5e973769f606aeb965f0ee4b39584
Charbel t <charbel2000.52@gmail.com>
2020-12-17 10:38:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ BoardMember.prototype.veto = ()=>'No, I must disagree';
 BoardMember.prototype.approve= ()=>'You can do that!';
 BoardMember.prototype.doCharity= ()=>'I like to help people.';
 BoardMember.prototype.releasePressStatement= ()=>'You will see great things from Scuber.';
-BoardMember.prototype.sayHi= ()=>`Hi, my name is ${BoardMember.name}. I am from ${BoardMember.homeState}, and I was trained in ${BoardMember.training}.`;
+BoardMember.prototype.sayHi= ()=>`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
